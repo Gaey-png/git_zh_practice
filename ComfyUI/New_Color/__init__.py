cca6a2df18b3cc58c9811be6a55058eac8252017
xiezhengyang <xiezhengyang@xiezhengyangs-MacBook-Pro.local>
2026-01-18 17:47:41 +0800

    Add new changes

---
+++ ComfyUI/New_Color/__init__.py
@@ -0,0 +1,12 @@
+from .NewColor import NewColorNode
+
+NODE_CLASS_MAPPINGS = {
+    "NewColor": NewColorNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "NewColor": "NewColor - 颜色调整"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
